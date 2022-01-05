45b764e034c786a091b3b82d53b93a9291aa0857
APRILIA <86938023+Toni33812@users.noreply.github.com>
2022-01-05 09:38:01 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@ var zahirr = db.get("hans");
 	console.log('')
 }
 
-var creatorList = ['@mszpros'];
+var creatorList = ['@aprilia'];
 var creator = creatorList[Math.floor(Math.random() * creatorList.length)];
 
 
